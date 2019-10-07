a7f333d6b4c1e159e5eb4cf14fb2368411ba7e9f
kasselTrankos <alvaro.touzon@gmail.com>
2019-10-07 23:37:14 +0200

    evolution travis

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for d in ./packages/*/ ; do (cd "$d" && npm i && npm test); done
+for d in ./packages/*/ ; do (cd "$d" && npm i && npm test -- -u && bash -c 'if [[ `git status --porcelain` ]]; then >&2 echo "Fail"; exit 1; fi'); done
