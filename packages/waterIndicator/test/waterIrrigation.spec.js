38ec5aba8674428cbd9baf1202908587639304de
kasselTrankos <alvaro.touzon@gmail.com>
2019-10-07 21:11:16 +0200

    give next generation

--- packages/waterIndicator/test/waterIrrigation.spec.js
+++ packages/waterIndicator/test/waterIrrigation.spec.js
@@ -1,24 +1,19 @@
 import 'react-native';
 import React from 'react';
-<<<<<<< HEAD
-import renderer from 'react-test-renderer';
-import WaterIrrigation from './../src';
-describe('<Testable>', () => {
-  it('renders the correct text', () => {
-    const value = 'greetings';
-    const pair = 'salutations';
-    console.log(WaterIrrigation);
-    const inst = renderer.create(
-      // <WaterIrrigation />
-    );
-=======
 import {create, act} from 'react-test-renderer';
 import {WaterIndicator} from './../src';
-describe('<Testable>', () => {
+import {StepsIrrigation} from './../src/steps';
+describe('<WaterIndicator>', () => {
   it('renders the correct text', () => {
     let root;
-    act(()=> root = create(<WaterIndicator />));
+    act(()=> {root = create(<WaterIndicator />);});
     expect(root.toJSON()).toMatchSnapshot();
->>>>>>> wall
+  });
+  it('render shallow read', ()=> {
+    const prop = [1, 4, 9];
+    const ins = create(<WaterIndicator steps={prop}/>);
+    const steps = ins.root.findByType(StepsIrrigation);
+    console.log(steps.props.steps);
+    expect(steps.props.steps).toEqual(prop);
   });
 });
