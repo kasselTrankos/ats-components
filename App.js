3a310fe3d22fdbfca21172c6d76957afba07d376
xe71086 <alvaro.touzon.contractor@bbva.com>
2020-01-20 12:31:21 +0100

    feat() made percent f consume water

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Circle from './src/Water';
+import Water from './src/Water';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={{
         width: 90,
-        height: 90,
+        height: '100%',
         // top: 0,
         // left: 0,
         // position: 'absolute',
         justifyContent: 'space-around',
         backgroundColor: '#FFF'
       }}>
-      <Circle />
+      <Water top="0%" />
 
       </View>
     </View>
--- src/Water.js
+++ src/Water.js
@@ -5,29 +5,30 @@ import {Svg, Circle, Defs, Mask, Path, Rect, Use} from 'react-native-svg';
 const Water = props => {
   const radius = 50;
   const {
-    background = '#000',
+    top ='0%',
+    background = '#EDC75C',
     borderWidth = 0,
     borderColor = '#333',
     
   } = props;
   return (
     <View style={{ aspectRatio: 1}}>
-      <Svg height="100%" width="100%" viewBox="0 0 100 100" style={{backgroundColor: '#eee', outline: '1px solid red'}}>
+      <Svg height="100%" width="100%" viewBox="0 0 100 100" style={{outline: '1px solid red'}}>
       <Defs>  
         <Mask 
           id="Mask" 
           x="0" 
-          y="0" 
+          y={top} 
           width="100%" 
           height="100%">  
           <Rect
             x="0"
             y="0"
             width="100%"
-            height="50"
-            fill="rgb(0,0,255)"
+            height="100%"
+            fill="#fff"
             strokeWidth="0"
-            stroke="rgb(0,0,0)"
+            stroke={background}
             maskUnits="userSpaceOnUse"
           />
         </Mask>  
@@ -35,10 +36,11 @@ const Water = props => {
           id="Circle"
           r={radius} 
           cx={radius} 
-          cy={radius} 
+          cy={radius}
+          fill-opacity="1"
           fill={background} />
       </Defs>
-      <Use href="#Circle" fill="green" mask="url(#Mask)" />
+      <Use href="#Circle" mask="url(#Mask)" />
       {/* <Circle 
         r={radius} 
         cx={radius} 
