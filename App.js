a5053386c256bbfba598549c68cc56ecfe1b4f39
kasselTrankos <alvaro.touzon@gmail.com>
2020-01-24 22:46:17 +0100

    spinner

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Svg, {Circle} from 'react-native-svg';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Svg height="100" width="100">
+        <Circle cx="50" cy="50" r="50" fill="pink" />
+      </Svg>
     </View>
   );
 }
