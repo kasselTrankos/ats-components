e2533b28a00aed565122c02eb537edf9cf4f347d
kasselTrankos <alvaro.touzon@gmail.com>
2020-01-24 10:28:05 +0100

    made disable as option

--- App.js
+++ App.js
@@ -3,23 +3,55 @@ import { StyleSheet, View } from 'react-native';
 import WaterManager from './src/WaterManager';
 
 
-export default function App() {
+export default function App(props) {
+  const {
+    disabled = false,
+    dialWidth = 1,
+    dialRadius = 40,
+    dialColor = "#3c70a4",
+    dialTextSize = 19,
+    dialTextColor = '#fff',
+    fillColor = 'none',
+    fontSize = 30,
+    fontColor = '#87c0cd',
+    maxDial = 360,
+    minDial = 0,
+    meterColor = '#333',
+    onChange = (e)=> {}, 
+    onPress = () => {console.log('is presssed')}, 
+    radius = 100,
+    sizes = [1, 2, 3, 'Empty'],
+    strokeWidth = 0,
+    strokeColor = '#d9eeec',
+    value = 120,
+    volumen = 100,
+    waterColor= '#eaf5ff',
+  } = props;
+
   return (
     <View style={styles.container}>
-      <WaterManager 
-      dialWidth = {8}
-      dialColor="#5BC0EB"
-      dialRadius={32}
-      dialTextSize={22}
-      dialTextColor= "#fff"
-      maxDial={60}
-      radius={140} 
-      strokeWidth={10} 
-      strokeColor="#3F88C5"
-      fontColor="#3F88C5"
-      fontSize={100}
-      value={0}
-      waterColor= "#A9CEF4"/>
+      <WaterManager
+      minDial={minDial}
+      onChange={onChange}
+      onPress={onPress}
+      sizes={sizes}
+      fillColor={fillColor}
+      volumen={volumen}
+      meterColor={meterColor}
+      disabled={disabled}
+      dialWidth = {dialWidth}
+      dialColor={dialColor}
+      dialRadius={dialRadius}
+      dialTextSize={dialTextSize}
+      dialTextColor= {dialTextColor}
+      maxDial={maxDial}
+      radius={radius} 
+      strokeWidth={strokeWidth} 
+      strokeColor={strokeColor}
+      fontColor={fontColor}
+      fontSize={fontSize}
+      value={value}
+      waterColor= {waterColor} />
     </View>
   );
 }
