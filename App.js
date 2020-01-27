b4d3d9114e9ff47d975f63e63f5e37fc422492f8
kasselTrankos <alvaro.touzon@gmail.com>
2020-01-27 22:41:02 +0100

    the necessary params

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 import { View } from 'react-native';
 import Calendar from "./src/Calendar.js";
 
-const onDates = (dates)=> {};
 
-const App = props => <View style={{top: 40, flex: 1}}>
+const App = props =>{ 
+  const {
+    rows = 7,
+    top = 100,
+    left = 0,
+    height = 340,
+    amount = 210,
+    width = '100%',
+    activeColor = '#2988B1',
+    colorDayText = '#192965',
+    inactiveColor = '#fff',
+    vibrationDuration= 100,
+    passedDay ='#0f4c75',
+    currentDay ='#edf7fa',
+    onDates= (e) => {}
+  } = props;
+  return <View style={{
+    flex: 1, 
+    position: 'absolute', 
+    height, top, width, left
+    }}>
     <Calendar
+      rows={rows}
       onDates={onDates}
-      inactiveColor="#f1f9f9"
-      colorDayText = "#192965"
-      passedDay = "#ebe6e6"
-      currentDay= "#9aceff" />
-  </View>
+      amount = {amount}
+      colorDayText = {colorDayText}
+      inactiveColor = {inactiveColor}
+      activeColor = {activeColor}
+      vibrationDuration = {vibrationDuration}
+      passedDay = {passedDay}
+      currentDay= {currentDay} />
+  </View>}
 
 export default App;
