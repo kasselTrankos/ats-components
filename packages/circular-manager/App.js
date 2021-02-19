a42f519b442b8263ccb9e71e1018dc50719bc8ca
kasselTrankos <alvaro.touzon@gmail.com>
2021-02-19 22:52:56 +0100

    feat: better

--- packages/circular-manager/App.js
+++ packages/circular-manager/App.js
@@ -57,12 +57,3 @@ export default function App(props) {
   );
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
